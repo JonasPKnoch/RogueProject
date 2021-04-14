b9f447b2f210560fe6651f856348d2b7c3943f1f
Jason Lee <watshsname@gmail.com>
2021-04-14 13:31:15 -0600

    Added == overload to point

--- Point.cs
+++ Point.cs
@@ -16,5 +16,40 @@ namespace DungeonGenerationDemo
 
         public int X { get; }
         public int Y { get; }
+
+
+        /// <summary>
+        /// Sees if the provided object is the same object as this
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(Object obj)
+        {
+            //Check for null and compare run-time types.
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Point p = (Point)obj;
+                return (X == p.X) && (Y == p.Y);
+            }
+        }
+
+        public static bool operator ==(Point x, Point y)
+        {
+            return x.X == y.X && x.Y == y.Y;
+        }
+
+        public static bool operator !=(Point x, Point y)
+        {
+            return !(x == y);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Point({0}, {1})", X, Y);
+        }
     }
 }
